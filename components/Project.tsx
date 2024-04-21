a99b1d9db9a0b803c424564ede12afe0113ae492
Pranto Mollick <prantocse01@gmail.com>
2024-04-21 21:01:18 +0600

    project section is done

---
+++ components/Project.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useRef } from "react";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+import { ProjectProps } from "./projects";
+
+export default function Project({
+  title,
+  description,
+  tags,
+  imageUrl,
+}: ProjectProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{
+        scale: scaleProgress,
+        opacity: opacityProgress,
+      }}
+      className="group mb-3 last:mb-0 sm:mb-8"
+    >
+      <section className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 sm:h-[20rem] sm:pr-8 sm:group-even:pl-8">
+        <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+          <ul className="mt-4 flex flex-wrap gap-2 sm:mt-auto">
+            {tags.map((tag, idx) => (
+              <li
+                className="rounded-full bg-black/70 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white"
+                key={idx}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Image
+          src={imageUrl}
+          alt={title}
+          quality={95}
+          className="absolute -right-40 top-8 w-[28.25rem] rounded-t-lg shadow-2xl transition group-even:-left-40 group-even:right-[initial] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
+        />
+      </section>
+    </motion.div>
+  );
+}
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./Project";
+export type ProjectProps = (typeof projectsData)[number];
 
 function Projects() {
   return (
     <section>
       <SectionHeading>My projects</SectionHeading>
-      <div></div>
+      <div>
+        {projectsData.map((project, idx) => (
+          <React.Fragment key={idx}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
   );
 }
